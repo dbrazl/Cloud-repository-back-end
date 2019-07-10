26bb454a34afea3b30d340adc90eaa81dba574fa
dbrazl <danielbrazlopes16@gmail.com>
2019-07-10 20:42:27 -0300

    feature: register user on db

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,41 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    /**
+     * Verify if the user exist in DB
+     */
+    const userExist = await User.findOne({ where: { email: req.body.email } });
+    if (userExist) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    /**
+     * Create user in DB
+     */
+    const { id, name, email } = await User.create(req.body);
+
+    /**
+     * Return response if HTTP 200
+     */
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async update(req, res) {
+    return res.json();
+  }
+
+  async index(req, res) {
+    return res.json();
+  }
+
+  async delete(req, res) {
+    return res.json();
+  }
+}
+
+export default new UserController();
