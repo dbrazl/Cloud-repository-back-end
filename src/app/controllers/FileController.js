ffd615800f4f3033a79b4f2770ddaaa61ed7c909
dbrazl <danielbrazlopes16@gmail.com>
2019-07-29 11:27:26 -0300

    chore: add schema validation to File update

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import File from '../models/File';
 
 class FileController {
@@ -24,13 +25,28 @@ class FileController {
   }
 
   async update(req, res) {
+    /**
+     * Schema validation
+     */
+    const schema = Yup.object().shape({
+      id: Yup.number().required(),
+      name: Yup.string().required(),
+    });
+
+    /**
+     * Verify if request is valid with our schema
+     */
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const { id } = req.body;
 
     const file = await File.findOne({ where: { id } });
 
     const { name } = await file.update(req.body);
 
-    res.json({ id, name });
+    return res.json({ id, name });
   }
 
   async delete(req, res) {
